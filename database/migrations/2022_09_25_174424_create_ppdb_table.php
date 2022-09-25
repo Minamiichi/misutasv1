396022cca7ddcd0d7e00160bdd64d0b0b306a9e8
Dwicky W P <dwicky1012@gmail.com>
2022-09-26 00:50:34 +0700

    tambah ppdb

---
+++ database/migrations/2022_09_25_174424_create_ppdb_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePpdbTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ppdb', function (Blueprint $table) {
+            $table->id();
+            $table->string('nisn');
+            $table->string('name');
+            $table->date('birthdate');
+            $table->string('gender');
+            $table->string('religion');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ppdb');
+    }
+}
